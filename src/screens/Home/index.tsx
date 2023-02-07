65ee00dfaa2fa6edb358175530951be7e2f8bc4a
Danilo Almeida Leite <danilo.a.leite@gmail.com>
2023-02-07 20:55:57 -0300

    feat: create state to save a new participant

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import {
   Text,
   View,
@@ -9,59 +10,70 @@ import {
 import { Partcipant } from '../../components/Participant';
 import { styles } from './styles';
 export function Home() {
-  const participants = [
-    'Danilo',
-    'Rodrigo',
-    'Vini',
-    'Biro',
-    'Rodrigo1',
-    'Vini1',
-    'Biro1',
-    'Rodrigo2',
-    'Vini2',
-    'Biro2',
-  ];
+  const [participantList, setParticipantList] = useState(['Danilo']);
+  const [inputInfo, setInputInfo] = useState('');
+
   function handleParticipantAdd() {
-    if (participants.includes('Rodrigo')) {
+    if (participantList.includes(inputInfo)) {
       return Alert.alert(
         'Participante existente',
-        'Ja exite um participante na lista com este nome!'
+        `${inputInfo} ja foi cadastrado na lista de participantes!`
       );
     }
-    console.log('Você clicou no botão adicionar!');
+
+    setParticipantList(() => [...participantList, inputInfo]);
+    setInputInfo(() => '');
   }
 
   function handleParticipantRemove(name: string) {
     Alert.alert('Remover', `Deseja remover o participante ${name}?`, [
       {
         text: 'Sim',
-        onPress: () => Alert.alert('Deletado!'),
+        onPress: () => {
+          let newList = participantList;
+          const index = participantList.indexOf(name);
+          if (index !== -1) {
+            newList.splice(index, 1);
+            setParticipantList(() => newList);
+            Alert.alert('Deletado!');
+          }
+        },
       },
       {
         text: 'Não',
         style: 'cancel',
       },
     ]);
-    console.log(`Você clicou no botão remover o participante ${name}!`);
+    console.log(participantList);
   }
+
+  useEffect(() => {
+    console.log(participantList);
+  }, [participantList]);
   return (
     <View style={styles.container}>
       <Text style={styles.enventName}>Nome do evento</Text>
       <Text style={styles.eventDate}>Nome do evento teste</Text>
       <View style={styles.form}>
         <TextInput
+          value={inputInfo}
           style={styles.input}
           placeholder="Nome do participante"
           placeholderTextColor="#6B6B6B"
-          keyboardType="numeric"
+          keyboardType="ascii-capable"
+          onChange={(e) => setInputInfo(e.nativeEvent.text.toString())}
         />
-        <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={handleParticipantAdd}
+          disabled={inputInfo === ''}
+        >
           <Text style={styles.buttonText}>+</Text>
         </TouchableOpacity>
       </View>
       <FlatList
         showsVerticalScrollIndicator={false}
-        data={participants}
+        data={participantList}
         keyExtractor={(item) => item}
         renderItem={({ item }) => (
           <Partcipant
